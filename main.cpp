bde2e7d8bc6624df65120112a33f832957cf539a
Spencer <spencerbetchey@gmail.com>
2026-06-17 13:02:55 -0500

    Updated main.cpp to test and compare BFS and Dijkstra

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "graph.h"
 #include "bfs.h"
+#include "dijkstra.h"
 
 int main() {
 
@@ -24,16 +25,32 @@ int main() {
 
     //Test Breadth-First Search
     BFS bfs;
+    vector<string> bfsPath = bfs.findPath(g, "A", "E");
+    bfs.printPath(bfsPath, "A", "E");
 
-    vector<string> path1 = bfs.findPath(g, "A", "E");
-    bfs.printPath(path1, "A", "E");
+    //Test Dijkstra
+    Dijkstra dijkstra;
+    vector<string> dijkstraPath = dijkstra.findPath(g, "A", "E");
 
-    vector<string> path2 = bfs.findPath(g, "B", "E");
-    bfs.printPath(path2, "B", "E");
+    //Calculate total cost of the path
+    int totalCost = 0;
+    const auto& adjList = g.getAdjacencyList();
+    for (int i = 0; i < dijkstraPath.size() - 1; i++) {
+        string current = dijkstraPath[i];
+        string next = dijkstraPath[i + 1];
+        for (const Edge& edge : adjList.at(current)) {
+            if (edge.neighbor == next) {
+                totalCost += edge.weight;
+                break;
+            }
+        }
+    }
+
+    dijkstra.printPath(dijkstraPath, "A", "E", totalCost);
 
     //Test with a vertex that doesn't exist
-    vector<string> path3 = bfs.findPath(g, "A", "Z");
-    bfs.printPath(path3, "A", "Z");
+    vector<string> badPath = dijkstra.findPath(g, "A", "Z");
+    dijkstra.printPath(badPath, "A", "Z", 0);
 
     return 0;
 }
